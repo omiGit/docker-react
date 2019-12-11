e87e6dc22a6a7229d876826d6cd7306d626da26a
omiGit <omi.github@gmail.com>
2019-12-12 02:41:38 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Application hello...
+          Omkar Jadhav
         </a>
       </header>
     </div>
